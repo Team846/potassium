4f4d7abc3a720a3f18e67c4b60ca5f6c703e9f7b
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-12-21 17:36:10 -0800

    Add documentation for components

--- core/src/main/scala/com/lynbrookrobotics/potassium/Component.scala
+++ core/src/main/scala/com/lynbrookrobotics/potassium/Component.scala
@@ -2,6 +2,18 @@ package com.lynbrookrobotics.potassium
 
 import squants.Time
 
+/**
+  * Represents a single robotic component, which translates signal data into action
+  *
+  * Components can be though of as a function of command => Unit, which
+  * is implemented in applySignal. The function is expected to take the latest
+  * command and send it to hardware interfaces. In addition, the applySignal method
+  * is the place to implement safeties, as it is the last layer of signal transformation.
+  *
+  * @param period the update rate of the component
+  * @param clock the clock to use to schedule periodic updates
+  * @tparam T the type of values produced by signals for the component
+  */
 abstract class Component[T](period: Time)(implicit val clock: Clock) {
   def defaultController: PeriodicSignal[T]
   private var currentController: PeriodicSignal[T] = defaultController
@@ -19,6 +31,8 @@ abstract class Component[T](period: Time)(implicit val clock: Clock) {
   def applySignal(signal: T): Unit
 
   clock(period) { dt =>
+    // Because of unusual initialization orders, currentController may sometimes be null,
+    // so we make sure to have it initialized here.
     if (currentController == null) {
       currentController = defaultController
     }
