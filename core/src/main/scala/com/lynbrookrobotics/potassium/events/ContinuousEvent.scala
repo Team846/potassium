eb6706ec5e469cb6d630da3ddf71381dd0df3d23
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-12-22 11:44:28 -0800

    Add documentation for events

--- core/src/main/scala/com/lynbrookrobotics/potassium/events/ContinuousEvent.scala
+++ core/src/main/scala/com/lynbrookrobotics/potassium/events/ContinuousEvent.scala
@@ -4,14 +4,31 @@ import com.lynbrookrobotics.potassium.clock.Clock
 import com.lynbrookrobotics.potassium.tasks.{ContinuousTask, Task}
 import squants.Time
 
+/**
+  * Contains configuration for continuous event condition polling
+  * @param clock the clock to use to periodically check the condition
+  * @param period the period to check the condition at
+  */
 case class EventPolling(clock: Clock, period: Time)
 
+/**
+  * An event that has a start, running, and ending phase.
+  * @param condition a boolean condition for when the event should be active
+  * @param polling the configuration for periodically checking if the event should be active
+  */
 class ContinuousEvent(condition: => Boolean)(implicit polling: EventPolling) {
-  val onStartSource = new ImpulseEventSource
-  val onEndSource = new ImpulseEventSource
+  private val onStartSource = new ImpulseEventSource
+  private val onEndSource = new ImpulseEventSource
 
-  val onStart = onStartSource.event
-  val onEnd = onEndSource.event
+  /**
+    * An event that is fired when the continuous event starts
+    */
+  val onStart: ImpulseEvent = onStartSource.event
+
+  /**
+    * An event that is fired when the continuous event ends
+    */
+  val onEnd: ImpulseEvent = onEndSource.event
 
   private var tickingCallbacks: List[() => Unit] = List.empty
   private var isRunning = false
@@ -31,10 +48,18 @@ class ContinuousEvent(condition: => Boolean)(implicit polling: EventPolling) {
     }
   }
 
+  /**
+    * Adds a listener to be called while the event is running
+    * @param onTicking a function to be called continuously when the event is running
+    */
   def foreach(onTicking: () => Unit): Unit = {
     tickingCallbacks = onTicking :: tickingCallbacks
   }
 
+  /**
+    * Adds a mapping to run a task while the continuous event is running
+    * @param task the task to run during the event
+    */
   def foreach(task: ContinuousTask): Unit = {
     onStart.foreach(() => Task.executeTask(task))
     onEnd.foreach(() => Task.abortTask(task))
--- core/src/main/scala/com/lynbrookrobotics/potassium/events/ImpluseEvent.scala
+++ core/src/main/scala/com/lynbrookrobotics/potassium/events/ImpluseEvent.scala
@@ -1,11 +1,26 @@
 package com.lynbrookrobotics.potassium.events
 
+/**
+  * A source of impulse events, which is used to trigger an event
+  *
+  * This is like [[scala.concurrent.Promise]] for [[scala.concurrent.Future]], where
+  * the source can be triggered with `fire()` and the event can be distributed to
+  * listeners.
+  */
 class ImpulseEventSource {
+  /**
+    * An event thats is triggered when `fire()` is called
+    */
   val event = new ImpulseEvent
 
   def fire(): Unit = event.fire()
 }
 
+/**
+  * An impulse event, which is fired and immediately ends
+  *
+  * Unlike continuous events, impulse events do not have a running phase
+  */
 class ImpulseEvent private[events]() {
   private var listeners: List[() => Unit] = List.empty
 
@@ -13,6 +28,10 @@ class ImpulseEvent private[events]() {
     listeners.foreach(_.apply())
   }
 
+  /**
+    * Adds a listener to be called when the impulse event is fired
+    * @param listener the listener to be fired
+    */
   def foreach(listener: () => Unit): Unit = {
     listeners = listener :: listeners
   }
