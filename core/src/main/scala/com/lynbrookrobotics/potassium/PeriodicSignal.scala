58855633a67656934bfd5df1b4aae221fc788fa1
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-12-21 17:30:53 -0800

    Add documentation for periodic signals

--- core/src/main/scala/com/lynbrookrobotics/potassium/PeriodicSignal.scala
+++ core/src/main/scala/com/lynbrookrobotics/potassium/PeriodicSignal.scala
@@ -2,16 +2,32 @@ package com.lynbrookrobotics.potassium
 
 import squants.Time
 
+/**
+  * Represents a signal that requires a fixed update interval
+  *
+  * This type of signal is used in cases where history must be tracked,
+  * as it prevents collisions between multiple value requests and includes
+  * a dt value for use in calculations.
+  *
+  * @tparam T the type of values the signal will produce
+  */
 abstract class PeriodicSignal[T] { self =>
   private var currentTickSource: Option[AnyRef] = None
 
-  val parent: Option[PeriodicSignal[_]]
-  val check: Option[T => Unit]
+  protected val parent: Option[PeriodicSignal[_]]
+  protected val check: Option[T => Unit]
 
   private var lastCalculated: Option[(T, Int)] = None
 
   protected def calculateValue(dt: Time, token: Int): T
 
+  /**
+    * Gets the latest value from the signal
+    * @param dt the time since the last value request
+    * @param requestToken an optional token to prevent recalculations of a
+    *                     value in a branched dependency
+    * @return the latest value of the signal
+    */
   def currentValue(dt: Time, requestToken: Int = PeriodicSignal.requestTokens.next()): T = {
     lastCalculated match {
       case Some((v, t)) if t == requestToken =>
@@ -25,6 +41,12 @@ abstract class PeriodicSignal[T] { self =>
     }
   }
 
+  /**
+    * Transforms the periodic signal by applying a function to all values
+    * @param f the function to transform values with
+    * @tparam U the type of the resulting signal
+    * @return a new signal with values transformed by the function
+    */
   def map[U](f: (T, Time) => U): PeriodicSignal[U] = new PeriodicSignal[U] {
     val parent = Some(self)
     val check = None
@@ -34,6 +56,12 @@ abstract class PeriodicSignal[T] { self =>
     }
   }
 
+  /**
+    * Combines the signal with another signal into a signal of tuples
+    * @param other the signal to combine with
+    * @tparam O the type of values of the other signal
+    * @return a new signal that returns tuples with one value from each signal
+    */
   def zip[O](other: PeriodicSignal[O]): PeriodicSignal[(T, O)] = new PeriodicSignal[(T, O)] {
     val parent = Some(self)
     val check = None
@@ -43,6 +71,11 @@ abstract class PeriodicSignal[T] { self =>
     }
   }
 
+  /**
+    * Creates a new periodic signal that returns the same value but also invokes the given callback
+    * @param checkCallback the callback to run with each value calculated by the signal
+    * @return a periodic signal with the callback invocations
+    */
   def withCheck(checkCallback: T => Unit): PeriodicSignal[T] = new PeriodicSignal[T] {
     val parent = Some(self)
     val check = Some(checkCallback)
@@ -50,6 +83,13 @@ abstract class PeriodicSignal[T] { self =>
     def calculateValue(dt: Time, token: Int): T = self.currentValue(dt, token)
   }
 
+  /**
+    * Marks the object that is producing ticks for the signal.
+    *
+    * This is needed in order to prevent tick collisions with multiple sources of ticks.
+    *
+    * @param source an object to mark as a tick source
+    */
   def attachTickSource(source: AnyRef): Unit = {
     if (currentTickSource.isEmpty) {
       currentTickSource = Some(source)
@@ -59,6 +99,10 @@ abstract class PeriodicSignal[T] { self =>
     }
   }
 
+  /**
+    * Removes any attached tick source from the signal and its parents.
+    * @param source the tick source to remove
+    */
   def detachTickSource(source: AnyRef): Unit = {
     currentTickSource.foreach { s =>
       if (s.eq(source)) {
