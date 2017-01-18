0b94c01e3b6a5b57c9e55ddf4d6d23a4eef4024d
Aquadrizzle <rinha.aqua@gmail.com>
2017-01-17 19:27:58 -0800

    Add more examples for LTI models

--- model/src/test/scala/com/lynbrookrobotics/potassium/model/examples/HangingSpring.scala
+++ model/src/test/scala/com/lynbrookrobotics/potassium/model/examples/HangingSpring.scala
@@ -7,30 +7,35 @@ import squants.mass.Kilograms
 import squants.motion.MetersPerSecondSquared
 import squants.time.Milliseconds
 
-object FallingMass extends App {
-  val NumIterations = 100
+object HangingSpring extends App {
+  val NumIterations = 3000
 
   val times     = new Array[Double](NumIterations)
   val positions = new Array[Double](NumIterations)
 
   val Mass = Kilograms(1) // kg
-  val GravityForce = Mass * MetersPerSecondSquared(9.8) // N
-  val dt = Milliseconds(5)
+  val GravityForce = Mass * MetersPerSecondSquared(-9.8) // N
+  val dt = Milliseconds(50)
 
   val input = DenseVector(GravityForce.toNewtons)
+
+  val k = 0.25
+  val m = 1.0
+
+  val springF = -k / m
 
   val stateSpace = new StateSpace(
     DenseMatrix(
       (0.0, 1.0),
-      (0.0, 0.0)
+      (springF, -0.1)
     ),
     DenseMatrix(
       0.0,
-      -1.0
+      1 / m
     )
   )
 
-  var state = DenseVector(0D, 0D)
+  var state = DenseVector(0.0, 0.0)
 
   for (i <- 0 until NumIterations) {
     times(i) = i * dt.toSeconds
