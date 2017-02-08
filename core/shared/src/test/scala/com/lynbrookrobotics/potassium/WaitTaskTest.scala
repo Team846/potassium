1a5cdb14d0f13d9f4d0a65db1508150b492f2c38
Aquadrizzle <rinha.aqua@gmail.com>
2017-02-07 21:06:58 -0800

    Add a task WaitTask Takes a time and waits that many units of time

---
+++ core/shared/src/test/scala/com/lynbrookrobotics/potassium/WaitTaskTest.scala
@@ -0,0 +1,20 @@
+package com.lynbrookrobotics.potassium
+
+import com.lynbrookrobotics.potassium.tasks.WaitTask
+import org.scalatest.FunSuite
+import squants.time.Seconds
+
+class WaitTaskTest extends FunSuite {
+  test("Wait tasks waits correct duration") {
+    implicit val (clock, trigger) = ClockMocking.mockedClockTicker
+    val waitTask = new WaitTask(Seconds(5))
+    waitTask.init()
+
+    val beforeTrigger = waitTask.isRunning
+    trigger(Seconds(5))
+    val afterTrigger = waitTask.isRunning
+
+    assert(beforeTrigger == true &&
+            afterTrigger == false)
+  }
+}
