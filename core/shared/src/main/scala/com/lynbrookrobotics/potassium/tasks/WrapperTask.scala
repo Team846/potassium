4b608ef27c5226b6b9cf75afcc7c6df17e1b6dc7
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-02-07 23:00:45 -0800

    Add wrapper tasks to wrap a task in another tasks's onStart and onEnd

---
+++ core/shared/src/main/scala/com/lynbrookrobotics/potassium/tasks/WrapperTask.scala
@@ -0,0 +1,87 @@
+package com.lynbrookrobotics.potassium.tasks
+
+sealed trait WrapperTaskState
+case object WrapperStopped extends WrapperTaskState
+case object WaitingForReady extends WrapperTaskState
+case object RunningInner extends WrapperTaskState
+
+abstract class WrapperTask { self =>
+  private var onReadyToRunInner: Option[() => Unit] = None
+
+  protected def readyToRunInner(): Unit = {
+    onReadyToRunInner.foreach(_.apply())
+  }
+
+  def onStart(): Unit
+  def onEnd(): Unit
+
+  def apply(inner: FiniteTask): FiniteTask = {
+    new FiniteTask with FiniteTaskFinishedListener {
+      private var state: WrapperTaskState = WrapperStopped
+
+      inner.addFinishedListener(this)
+
+      def onStart(): Unit = {
+        state = WaitingForReady
+
+        self.onReadyToRunInner = Some(() => {
+          if (state == WaitingForReady) {
+            state = RunningInner
+            inner.init()
+          }
+        })
+
+        self.onStart()
+      }
+
+      def onEnd(): Unit = {
+        self.onEnd()
+
+        if (state == RunningInner) {
+          inner.abort()
+        }
+
+        self.onReadyToRunInner = None
+
+        state = WrapperStopped
+      }
+
+      override def onFinished(task: FiniteTask): Unit = {
+        if (task == inner && state == RunningInner) {
+          finished()
+        }
+      }
+    }
+  }
+
+  def apply(inner: ContinuousTask): ContinuousTask = {
+    new ContinuousTask {
+      private var state: WrapperTaskState = WrapperStopped
+
+      def onStart(): Unit = {
+        state = WaitingForReady
+
+        self.onReadyToRunInner = Some(() => {
+          if (state == WaitingForReady) {
+            state = RunningInner
+            inner.init()
+          }
+        })
+
+        self.onStart()
+      }
+
+      def onEnd(): Unit = {
+        if (state == RunningInner) {
+          inner.abort()
+        }
+
+        self.onEnd()
+
+        self.onReadyToRunInner = None
+
+        state = WrapperStopped
+      }
+    }
+  }
+}
---
+++ core/shared/src/test/scala/com/lynbrookrobotics/potassium/WrapperTaskTest.scala
@@ -0,0 +1,250 @@
+package com.lynbrookrobotics.potassium
+
+import com.lynbrookrobotics.potassium.tasks.{ContinuousTask, FiniteTask, WrapperTask}
+import org.scalatest.FunSuite
+
+class WrapperTaskTest extends FunSuite {
+  test("Wrapper task goes through correct flow for finite inner task") {
+    var finiteTaskFinishTrigger: Option[() => Unit] = None
+
+    var finiteTaskStarted = false
+    var finiteTaskEnded = false
+
+    val finiteTask = new FiniteTask {
+      override def onStart(): Unit = {
+        finiteTaskStarted = true
+      }
+
+      override def onEnd(): Unit = {
+        finiteTaskEnded = true
+      }
+
+      finiteTaskFinishTrigger = Some(() => finished())
+    }
+
+    var wrapperTaskFinishTrigger: Option[() => Unit] = None
+
+    var wrapperTaskStarted = false
+    var wrapperTaskEnded = false
+
+    val wrapper = new WrapperTask {
+      override def onStart() = {
+        wrapperTaskStarted = true
+      }
+
+      override def onEnd() = {
+        wrapperTaskEnded = true
+      }
+
+      wrapperTaskFinishTrigger = Some(() => readyToRunInner())
+    }
+
+    val wrappedTask = wrapper(finiteTask)
+
+    assert(!finiteTaskStarted && !finiteTaskEnded && !wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.init()
+
+    assert(!finiteTaskStarted && !finiteTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrapperTaskFinishTrigger.get.apply()
+
+    assert(finiteTaskStarted && !finiteTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    finiteTaskFinishTrigger.get.apply()
+
+    assert(finiteTaskStarted && finiteTaskEnded && wrapperTaskStarted && wrapperTaskEnded)
+  }
+
+  test("Wrapper task aborts correctly when finite inner task has not started") {
+    var finiteTaskFinishTrigger: Option[() => Unit] = None
+
+    var finiteTaskStarted = false
+    var finiteTaskEnded = false
+
+    val finiteTask = new FiniteTask {
+      override def onStart(): Unit = {
+        finiteTaskStarted = true
+      }
+
+      override def onEnd(): Unit = {
+        finiteTaskEnded = true
+      }
+
+      finiteTaskFinishTrigger = Some(() => finished())
+    }
+
+    var wrapperTaskFinishTrigger: Option[() => Unit] = None
+
+    var wrapperTaskStarted = false
+    var wrapperTaskEnded = false
+
+    val wrapper = new WrapperTask {
+      override def onStart() = {
+        wrapperTaskStarted = true
+      }
+
+      override def onEnd() = {
+        wrapperTaskEnded = true
+      }
+
+      wrapperTaskFinishTrigger = Some(() => readyToRunInner())
+    }
+
+    val wrappedTask = wrapper(finiteTask)
+
+    assert(!finiteTaskStarted && !finiteTaskEnded && !wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.init()
+
+    assert(!finiteTaskStarted && !finiteTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.abort()
+
+    assert(!finiteTaskStarted && !finiteTaskEnded && wrapperTaskStarted && wrapperTaskEnded)
+  }
+
+  test("Wrapper task aborts correctly when finite inner task has already started") {
+    var finiteTaskFinishTrigger: Option[() => Unit] = None
+
+    var finiteTaskStarted = false
+    var finiteTaskEnded = false
+
+    val finiteTask = new FiniteTask {
+      override def onStart(): Unit = {
+        finiteTaskStarted = true
+      }
+
+      override def onEnd(): Unit = {
+        finiteTaskEnded = true
+      }
+
+      finiteTaskFinishTrigger = Some(() => finished())
+    }
+
+    var wrapperTaskFinishTrigger: Option[() => Unit] = None
+
+    var wrapperTaskStarted = false
+    var wrapperTaskEnded = false
+
+    val wrapper = new WrapperTask {
+      override def onStart() = {
+        wrapperTaskStarted = true
+      }
+
+      override def onEnd() = {
+        wrapperTaskEnded = true
+      }
+
+      wrapperTaskFinishTrigger = Some(() => readyToRunInner())
+    }
+
+    val wrappedTask = wrapper(finiteTask)
+
+    assert(!finiteTaskStarted && !finiteTaskEnded && !wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.init()
+
+    assert(!finiteTaskStarted && !finiteTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrapperTaskFinishTrigger.get.apply()
+
+    assert(finiteTaskStarted && !finiteTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.abort()
+
+    assert(finiteTaskStarted && finiteTaskEnded && wrapperTaskStarted && wrapperTaskEnded)
+  }
+
+  test("Wrapper task goes through correct flow for continuous inner task") {
+    var innerTaskStarted = false
+    var innerTaskEnded = false
+
+    val innerTask = new ContinuousTask {
+      override def onStart(): Unit = {
+        innerTaskStarted = true
+      }
+
+      override def onEnd(): Unit = {
+        innerTaskEnded = true
+      }
+    }
+
+    var wrapperTaskFinishTrigger: Option[() => Unit] = None
+
+    var wrapperTaskStarted = false
+    var wrapperTaskEnded = false
+
+    val wrapper = new WrapperTask {
+      override def onStart() = {
+        wrapperTaskStarted = true
+      }
+
+      override def onEnd() = {
+        wrapperTaskEnded = true
+      }
+
+      wrapperTaskFinishTrigger = Some(() => readyToRunInner())
+    }
+
+    val wrappedTask = wrapper(innerTask)
+
+    assert(!innerTaskStarted && !innerTaskEnded && !wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.init()
+
+    assert(!innerTaskStarted && !innerTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrapperTaskFinishTrigger.get.apply()
+
+    assert(innerTaskStarted && !innerTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.abort()
+
+    assert(innerTaskStarted && innerTaskEnded && wrapperTaskStarted && wrapperTaskEnded)
+  }
+
+  test("Wrapper task aborts correctly when continuous inner task has not started") {
+    var innerTaskStarted = false
+    var innerTaskEnded = false
+
+    val innerTask = new ContinuousTask {
+      override def onStart(): Unit = {
+        innerTaskStarted = true
+      }
+
+      override def onEnd(): Unit = {
+        innerTaskEnded = true
+      }
+    }
+
+    var wrapperTaskFinishTrigger: Option[() => Unit] = None
+
+    var wrapperTaskStarted = false
+    var wrapperTaskEnded = false
+
+    val wrapper = new WrapperTask {
+      override def onStart() = {
+        wrapperTaskStarted = true
+      }
+
+      override def onEnd() = {
+        wrapperTaskEnded = true
+      }
+
+      wrapperTaskFinishTrigger = Some(() => readyToRunInner())
+    }
+
+    val wrappedTask = wrapper(innerTask)
+
+    assert(!innerTaskStarted && !innerTaskEnded && !wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.init()
+
+    assert(!innerTaskStarted && !innerTaskEnded && wrapperTaskStarted && !wrapperTaskEnded)
+
+    wrappedTask.abort()
+
+    assert(!innerTaskStarted && !innerTaskEnded && wrapperTaskStarted && wrapperTaskEnded)
+  }
+}
