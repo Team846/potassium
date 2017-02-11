6aa8165add4e904f55bd315127e85f89a4866e33
Sang Hak (Andy) Chun <sanghak@andychun.info>
2017-02-10 20:54:08 -0800

    Add timestamped CameraSignal varaiant

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/CameraSignal.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/CameraSignal.scala
@@ -5,6 +5,8 @@ import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
 import com.lynbrookrobotics.potassium.Signal
 
+case class TimestampedMat(mat: Mat, timestamp: Double)
+
 class CameraSignal(camera: VideoCapture, frameWidth: Int = 320, frameHeight: Int = 200) extends Signal[Mat] {
   camera.set(Videoio.CAP_PROP_FRAME_WIDTH, frameWidth)
   camera.set(Videoio.CAP_PROP_FRAME_HEIGHT, frameHeight)
@@ -14,5 +16,13 @@ class CameraSignal(camera: VideoCapture, frameWidth: Int = 320, frameHeight: Int
   override def get() = {
     camera.read(mat)
     mat
+  }
+}
+
+class TimestampedCameraSignal(camera: VideoCapture, frameWidth: Int = 320, frameHeight: Int = 200) extends Signal[TimestampedMat] {
+  val signal = new CameraSignal(camera, frameWidth, frameHeight)
+
+  override def get() = {
+    new TimestampedMat(signal.get, System.nanoTime() / 1E9)
   }
 }
