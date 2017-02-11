88a7e5e1c0691a48c60b449728e7245a47107d48
Sang Hak (Andy) Chun <sanghak@andychun.info>
2017-02-10 21:14:26 -0800

    Take timestamp before decoding frame

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/CameraSignal.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/CameraSignal.scala
@@ -20,9 +20,15 @@ class CameraSignal(camera: VideoCapture, frameWidth: Int = 320, frameHeight: Int
 }
 
 class TimestampedCameraSignal(camera: VideoCapture, frameWidth: Int = 320, frameHeight: Int = 200) extends Signal[TimestampedMat] {
-  val signal = new CameraSignal(camera, frameWidth, frameHeight)
+  camera.set(Videoio.CAP_PROP_FRAME_WIDTH, frameWidth)
+  camera.set(Videoio.CAP_PROP_FRAME_HEIGHT, frameHeight)
+
+  val mat = new Mat()
 
   override def get() = {
-    new TimestampedMat(signal.get, System.nanoTime() / 1E9)
+    camera.grab()
+    val timestamp = System.nanoTime / 1e9
+    camera.retrieve(mat)
+    new TimestampedMat(mat, timestamp)
   }
 }
