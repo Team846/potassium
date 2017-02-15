570fea0e0671f448843461c4ed0405be3d08870e
dtjong <davintjong@gmail.com>
2017-02-14 21:01:41 -0800

    Add proximity sensor wrapper (#57)

---
+++ frc/src/main/scala/com/lynbrookrobotics/potassium/frc/ProximitySensor.scala
@@ -0,0 +1,22 @@
+package com.lynbrookrobotics.potassium.frc
+
+import edu.wpi.first.wpilibj.AnalogInput
+import squants.Length
+
+/**
+  * Implements interface for sharp IR sensor
+  * datasheet:https://acroname.com/articles/linearizing-sharp-ranger-data
+  * @param channel
+  */
+class ProximitySensor(channel: Int) {
+  val sensor:AnalogInput = new AnalogInput(channel)
+
+  def getVoltage: Double = {
+    sensor.getAverageVoltage
+  }
+
+  def isCloserThan(distance: Length): Boolean = {
+    var minVoltage:Double = 1/(distance.toCentimeters + 0.42)
+    getVoltage > minVoltage
+  }
+}
