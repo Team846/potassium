eca5eb3e867cc2a53a027bb410a1a19bc6a503da
wafflemans <yang.timothy2000@gmail.com>
2017-02-20 15:18:02 -0800

    Added a task that ends when a given impulse event is triggered

---
+++ core/shared/src/main/scala/com/lynbrookrobotics/potassium/tasks/WaitForImpulseTask.scala
@@ -0,0 +1,12 @@
+package com.lynbrookrobotics.potassium.tasks
+
+import com.lynbrookrobotics.potassium.events.ImpulseEvent
+
+class WaitForImpulseTask(impulseEvent: ImpulseEvent) extends FiniteTask{
+
+  impulseEvent.foreach(() => finished())
+  
+  override protected def onStart(): Unit = {}
+
+  override protected def onEnd(): Unit = {}
+}
