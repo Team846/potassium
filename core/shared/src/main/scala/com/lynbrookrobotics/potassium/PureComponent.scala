56ee01d6d9b357dc531c08b2de063afa02be7448
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-04-09 14:04:15 -0700

    Add shouldComponentUpdate to minimize component updates

---
+++ core/shared/src/main/scala/com/lynbrookrobotics/potassium/PureComponent.scala
@@ -0,0 +1,11 @@
+package com.lynbrookrobotics.potassium
+
+/**
+  * A component that decides to update by checking for equality between old and new signals
+  * @tparam T the type of values produced by signals for the component
+  */
+trait PureComponent[T] extends Component[T] {
+  override def shouldComponentUpdate(previousSignal: T, newSignal: T): Boolean = {
+    previousSignal != newSignal
+  }
+}
