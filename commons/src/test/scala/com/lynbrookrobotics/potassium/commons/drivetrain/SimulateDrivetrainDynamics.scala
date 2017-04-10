def7e04acdaa8f18867200ca4f5dbf43fbc7d4a3
Philip Axelrod <philip.axelrod.web@gmail.com>
2017-04-10 11:40:26 -0700

    Add drivetrain hardware and component to simulate driving physics. Initial commit, with much, much refactoring to TODO

---
+++ commons/src/test/scala/com/lynbrookrobotics/potassium/commons/drivetrain/SimulateDrivetrainDynamics.scala
@@ -0,0 +1,75 @@
+package com.lynbrookrobotics.potassium.commons.drivetrain
+
+
+import com.lynbrookrobotics.potassium.control.PIDConfig
+import com.lynbrookrobotics.potassium.testing.ClockMocking
+import com.lynbrookrobotics.potassium.units.GenericValue._
+import com.lynbrookrobotics.potassium.units._
+import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal, SignalLike}
+import squants.motion.{AngularVelocity, DegreesPerSecond, MetersPerSecond, MetersPerSecondSquared}
+import squants.motion._
+import squants.space.{Degrees, Feet, Meters}
+import squants.time.{Milliseconds, Seconds}
+import squants.{Acceleration, Angle, Length, Percent, Velocity}
+import org.scalatest.FunSuite
+/**
+  * Created by Philip on 4/9/2017.
+  */
+class SimulateDrivetrainDynamics extends FunSuite {
+  implicit val drivetrainPackage = new UnicyclePackageSimulator
+  val props = Signal.constant(new UnicycleProperties {
+    override val maxForwardVelocity: Velocity = MetersPerSecond(10)
+    override val maxTurnVelocity: AngularVelocity = DegreesPerSecond(10)
+    override val maxAcceleration: Acceleration = FeetPerSecondSquared(15)
+    override val defaultLookAheadDistance: Length = Meters(3)
+
+    override val forwardControlGains = PIDConfig(
+      Percent(10) / MetersPerSecond(1),
+      Percent(0) / Meters(1),
+      Percent(0) / MetersPerSecondSquared(1)
+    )
+
+    override val turnControlGains = PIDConfig(
+      Percent(10) / DegreesPerSecond(1),
+      Percent(0) / Degrees(1),
+      Percent(0) / (DegreesPerSecond(1).toGeneric / Seconds(1))
+    )
+
+    override val forwardPositionControlGains = PIDConfig(
+      Percent(100) / Meters(1),
+      Percent(0) / (Meters(1).toGeneric * Seconds(1)),
+      Percent(0) / MetersPerSecond(1)
+    )
+
+    override val turnPositionControlGains = PIDConfig(
+      Percent(100) / Degrees(10),
+      Percent(0) / (Degrees(1).toGeneric * Seconds(1)),
+      Percent(0) / DegreesPerSecond(1)
+    )
+  })
+
+  implicit val hardware = new SimulatedUnicycleHardware(props.get, Feet(2))
+
+  test("Test drive forward controller oscilates about end point") {
+    implicit val (clock, ticker) = ClockMocking.mockedClockTicker
+
+    implicit val simulatedComponent = new SimulatedUnicycleDriveComponent(Milliseconds(5))(hardware, clock)
+    val task = new drivetrainPackage.unicycleTasks.DriveDistanceStraight(
+      Meters(1),
+      Meters(0.0),
+      Degrees(5),
+      Percent(100))(simulatedComponent, hardware, props)
+    task.init()
+
+    for (_ <- 1 to 3000) {
+      ticker(Milliseconds(5))
+    }
+    println(hardware.history.last)
+    for (i <- 1 to 3000 by 10) {
+      println(s"${hardware.history(i)._1.toSeconds}\t ${hardware.history(i)._2.toFeet}")
+    }
+
+
+    assert(task.isRunning, "finished with this history " + hardware.history.mkString)
+  }
+}
---
+++ commons/src/test/scala/com/lynbrookrobotics/potassium/commons/drivetrain/SimulatedUnicycleHardware.scala
@@ -0,0 +1,223 @@
+package com.lynbrookrobotics.potassium.commons.drivetrain
+import com.lynbrookrobotics.potassium.clock.Clock
+import com.lynbrookrobotics.potassium.control.PIDF
+import com.lynbrookrobotics.potassium.units.{GenericDerivative, Ratio}
+import com.lynbrookrobotics.potassium.{Component, PeriodicSignal, Signal, SignalLike}
+import org.scalatest.time.Milliseconds
+import squants.Quantity
+import squants.{Acceleration, Angle, Dimensionless, Length, Percent, Velocity}
+import squants.motion._
+import squants.space.{Degrees, Feet, Meters}
+import squants.time.{Seconds, Time, TimeDerivative}
+
+import scala.collection.mutable
+
+/**
+  * Simulate inputs and motor back emf (friction) to model kinematics of
+  * a unicycleDrivetrain
+  */
+class SimulatedUnicycleHardware(
+    maxAcceleration: Acceleration,
+    maxTurnAcceleration: GenericDerivative[AngularVelocity],
+    maxVelocity: Velocity,
+    maxTurnVelocity: AngularVelocity) extends UnicycleHardware {
+
+  def this(maxAcceleration: Acceleration, maxVelocity: Velocity, track: Length) {
+    this(
+      maxAcceleration,
+      unitTools.linearToAngular(maxAcceleration, track),
+      maxVelocity,
+      unitTools.linearToAngular(maxVelocity, track))
+  }
+
+  def this(properties: UnicycleProperties, track: Length) {
+    this(properties.maxAcceleration, properties.maxForwardVelocity, track)
+  }
+
+  private def capAt100Percent(input: Dimensionless) = {
+    input min Percent(100) max Percent(-100)
+  }
+
+  val history = new mutable.ArrayBuffer[(Time, Length, Angle, Velocity, AngularVelocity)]
+  var time: Time = Seconds(0)
+
+  def clearHistory() {
+    history.clear()
+    time = Seconds(0)
+  }
+
+  def updateHistory() {
+    history.append(
+      (
+        time,
+        forwardPosition.get,
+        turnPosition.get,
+        forwardVelocity.get,
+        turnVelocity.get
+      ))
+  }
+  var lastOutput: UnicycleSignal = _
+
+  def updateData(dt: Time): Unit = {
+    periodicForwardPosition.currentValue(dt)
+    periodicTurnPosition.currentValue(dt)
+  }
+  /**
+    * component's applySignal method should call this method
+    * @param unicycleSignal
+    */
+  def acceptInput(unicycleSignal: UnicycleSignal, period: Time) {
+    lastOutput = unicycleSignal
+    updateHistory()
+    updateData(period)
+  }
+
+  var inputForwardAcceleration = Signal(capAt100Percent(lastOutput.forward).toEach * maxAcceleration).toPeriodic
+
+  val periodicVelocity = inputForwardAcceleration.scanLeft(FeetPerSecond(0)){
+    case (velocity, inputAcceleration, dt) =>
+      time += dt
+      // motor back emf is proportional to speed
+      val frictionAcceleration = -1 * maxAcceleration * (velocity / maxVelocity)
+
+      // newton's second law
+      val acceleration = inputAcceleration + frictionAcceleration
+
+      velocity + acceleration * dt
+  }
+
+//  def incrementVelocity[T <: Quantity[T]](accum: GenericDerivative[T],
+//                           maxVelocity: GenericDerivative[T],
+//                           inputAcceleration: GenericDerivative[GenericDerivative[T]],
+//                           maxAcceleration: GenericDerivative[GenericDerivative[T]],
+//                           dt: Time): GenericDerivative[T] = {
+//    val frictionAcceleration = maxAcceleration * (accum / maxVelocity)
+//
+//    val acceleration = if (frictionAcceleration.abs > inputAcceleration.abs) {
+//
+//      // friction is always in the opposite direction of motion
+//      if (frictionAcceleration.value.signum != accum.value.signum) {
+//        inputAcceleration - frictionAcceleration
+//      } else {
+//        MetersPerSecondSquared(0)
+//      }
+//    } else {
+//      inputAcceleration - frictionAcceleration
+//    }
+//
+//    (acceleration * dt).asInstanceOf
+//  }
+
+  override val forwardVelocity = peekVelocity(periodicVelocity)
+
+  var lastTurnAcceleration = Signal(maxTurnAcceleration * capAt100Percent(lastOutput.turn).toEach)
+
+  val periodicTurnVelocity = lastTurnAcceleration.toPeriodic.scanLeft(DegreesPerSecond(0)){
+    case (velocity, inputAcceleration, dt) =>
+      val frictionAcceleration = maxTurnAcceleration * (velocity / maxTurnVelocity)
+
+      val acceleration = inputAcceleration + frictionAcceleration
+
+      velocity + acceleration * dt
+  }
+
+  override val turnVelocity = peekTurnVelocity(periodicTurnVelocity)
+
+  val periodicForwardPosition = periodicVelocity.scanLeft(Meters(0)) {
+    case (acc, velocity, dt) => acc + velocity * dt
+  }
+  override val forwardPosition = peekLength(periodicForwardPosition)
+
+  val periodicTurnPosition = periodicTurnVelocity.scanLeft(Degrees(0)) {
+    case (acc, velocity, dt) => acc + velocity * dt
+  }
+  override val turnPosition = peekAngle(periodicTurnPosition)
+
+  def peekAngle(toPeek: PeriodicSignal[Angle]): Signal[Angle] = {
+    toPeek.peek.map(_.getOrElse(Degrees(0)))
+  }
+
+  def peekLength(toPeek: PeriodicSignal[Length]): Signal[Length] = {
+    toPeek.peek.map(_.getOrElse(Meters(0)))
+  }
+
+  def peekTurnVelocity(toPeek: PeriodicSignal[AngularVelocity]) = {
+    toPeek.peek.map(_.getOrElse(DegreesPerSecond(0)))
+  }
+
+  def peekVelocity(toPeek: PeriodicSignal[Velocity]) = {
+    toPeek.peek.map(_.getOrElse(MetersPerSecond(0)))
+  }
+}
+
+object unitTools {
+  def linearToAngular(speed: Velocity, radius: Length): AngularVelocity = {
+    RadiansPerSecond(speed.toMetersPerSecond / radius.toMeters)
+  }
+
+  def linearToAngular(acceleration: Acceleration, radius: Length) = {
+    new GenericDerivative(
+      acceleration.toMetersPerSecondSquared / radius.toMeters,
+      RadiansPerSecond
+    )
+  }
+}
+
+class UnicyclePackageSimulator extends UnicycleDrive {
+  override type Hardware = SimulatedUnicycleHardware
+  override type Properties = UnicycleProperties
+
+  override type DriveSignal = UnicycleSignal
+  override type DriveVelocity = UnicycleSignal
+  override protected def convertUnicycleToDrive(uni: UnicycleSignal): DriveSignal = uni
+
+
+  override type Drivetrain = SimulatedUnicycleDriveComponent
+
+  override protected def controlMode(implicit hardware: SimulatedUnicycleHardware, props: UnicycleProperties): UnicycleControlMode = ???
+
+  /**
+    * Drives with the signal with closed-loop control
+    *
+    * @param signal the signal to drive with
+    * @return
+    */
+  override protected def driveClosedLoop(signal: SignalLike[UnicycleSignal])
+                                        (implicit hardware: SimulatedUnicycleHardware,
+                                        props: Signal[UnicycleProperties]): PeriodicSignal[UnicycleSignal] = {
+    val targetForwardVelocity = signal.map(_.forward.toEach * props.get.maxForwardVelocity)
+    val targetTurnVelocity = signal.map(_.turn.toEach * props.get.maxTurnVelocity)
+
+    val forwardOutput = PIDF.pidf(
+      hardware.forwardVelocity.toPeriodic,
+      targetForwardVelocity.toPeriodic,
+      props.map(_.forwardControlGainsFull))
+    val turnOuput = PIDF.pid(
+      hardware.turnVelocity.toPeriodic,
+      targetTurnVelocity.toPeriodic,
+      props.map(_.turnControlGains))
+
+    forwardOutput.zip(turnOuput).map(o => UnicycleSignal(o._1, o._2))
+  }
+}
+
+class SimulatedUnicycleDriveComponent(period: Time)
+                                      (implicit hardware: SimulatedUnicycleHardware,
+                                                clock: Clock) extends Component[UnicycleSignal](period) {
+  override def defaultController: PeriodicSignal[UnicycleSignal] = Signal.constant(
+    UnicycleSignal(Percent(0), Percent(0))
+  ).toPeriodic
+//
+//  override def setController(controller: PeriodicSignal[UnicycleSignal]): Unit = {
+//    super.setController(controller.withCheck((d) => hardware.updateData(Seconds(5))))
+//  }
+
+  /**
+    * Applies the latest control signal value.
+    *
+    * @param signal the signal value to act on
+    */
+  override def applySignal(signal: UnicycleSignal): Unit = {
+    hardware.acceptInput(signal, Seconds(0.005))
+  }
+}
