4e96d0d71ce4cb2e475daa462c77fe7de30cc093
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-06-23 14:56:45 -0700

    Automatic publishing to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,14 @@ set -e # exit with nonzero exit code if anything fails
 
 # install ssh keys
 openssl aes-256-cbc -K $encrypted_1df6839761c3_key -iv $encrypted_1df6839761c3_iv -in .travisdeploykey.enc -out .travisdeploykey -d
+openssl aes-256-cbc -K $encrypted_2aaf6d71beae_key -iv $encrypted_2aaf6d71beae_iv -in travis-deploy-key.enc -out travis-deploy-key -d
+
 chmod go-rwx .travisdeploykey
+chmod go-rwx travis-deploy-key
+
 eval `ssh-agent -s`
 ssh-add .travisdeploykey
+ssh-add travis-deploy-key
 
 # clone the current gh-pages branch into the repo folder
 git clone -b gh-pages --single-branch git@github.com:Team846/repo.git repo
@@ -30,3 +35,5 @@ git commit -m "Automated deploy to GitHub Pages"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 git push --force --quiet "git@github.com:Team846/repo.git" gh-pages:gh-pages > /dev/null 2>&1
+
+sbt docs/publishMicrosite
