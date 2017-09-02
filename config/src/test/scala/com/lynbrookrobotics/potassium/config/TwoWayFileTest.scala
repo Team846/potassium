6372b58a6bc5b8677ba048e1c78822a8bf414227
philip <philip.axelrod.web@gmail.com>
2017-09-01 20:02:09 -0700

    Undo intellij's "safe" delete of unit tests :c

--- config/src/test/scala/com/lynbrookrobotics/potassium/config/TwoWayFileTest.scala
+++ config/src/test/scala/com/lynbrookrobotics/potassium/config/TwoWayFileTest.scala
@@ -1,22 +1,22 @@
 package com.lynbrookrobotics.potassium.config
 
-import com.lynbrookrobotics.potassium.config.TwoWayFile
 import java.io.{File, PrintWriter}
 import java.nio.file.Files
 
 import squants.motion.{MetersPerSecond, Velocity}
-import SquantsPickling._
 import org.scalatest.FunSuite
 import upickle.default._
 
 
-class Test extends FunSuite {
+class TwoWayFileTest extends FunSuite {
   case class RobotConfig(drive: DriveConfig)
   case class DriveConfig(maxForwardSpeed: Velocity)
 
-  test("Test writing and reading randomly generated values to config") {
+  test("Test writingu and reading randomly generated values to config") {
     val file = new File("test.txt")
-    file.createNewFile()
+    if (!file.exists()) {
+      Files.createFile(file.toPath)
+    }
 
     // initial value is required in config file
     val initConfig = write[RobotConfig](null)
