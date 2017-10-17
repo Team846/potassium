d9e7fc334d5c48f2b3c1981a756cde9dd103b895
Kunal Sheth <kunal@kunalsheth.info>
2017-10-16 17:52:38 -0700

    Take tick as parameter

--- commons/src/main/scala/com/lynbrookrobotics/potassium/commons/electronics/DynamicBraking.scala
+++ commons/src/main/scala/com/lynbrookrobotics/potassium/commons/electronics/DynamicBraking.scala
@@ -23,21 +23,18 @@ object DynamicBraking {
     .map(_.toCharArray)
     .map(_.map(_ == '|'))
   private val ditherLength = ditherPatterns.map(_.length).min
-  private var tick = Byte.MinValue
 
   def orBrake(max: ElectricPotential, free: AngularVelocity)
-             (target: ElectricPotential, current: AngularVelocity)
+             (tick: Int, target: ElectricPotential, current: AngularVelocity)
   : Option[ElectricPotential] = {
     val sp = target / max
     val pv = current / free
-    if (QuantityRange(0, pv.abs) contains sp) {
-      tick += 1
+    if (QuantityRange(0, pv.abs) contains sp)
       if (ditherPatterns
       (((pv / sp) * ditherPatterns.length).toInt)
       (tick.abs % ditherLength)
       ) Some(Volts(0))
       else None
-    }
     else if (QuantityRange(
       math.min(sp, pv),
       math.max(sp, pv)
