13832bcba91b09b0adcafe50dea7cb7ef91886cc
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-11-18 11:44:27 -0800

    Generate more accurate patterns on-the-fly instead of hardcoding

--- commons/src/main/scala/com/lynbrookrobotics/potassium/commons/electronics/DynamicBraking.scala
+++ commons/src/main/scala/com/lynbrookrobotics/potassium/commons/electronics/DynamicBraking.scala
@@ -1,49 +1,63 @@
 package com.lynbrookrobotics.potassium.commons.electronics
 
-import squants.electro.{ElectricPotential, Volts}
-import squants.motion.AngularVelocity
+import com.lynbrookrobotics.potassium.streams.Stream
+import squants.{Dimensionless, Each, Percent}
+
+sealed trait DitherStatus
+case class NotBraking(outputPower: Dimensionless) extends DitherStatus
+case class ApplyBrakingPattern(brakingPower: Dimensionless) extends DitherStatus
 
 object DynamicBraking {
-  // each String represents a duty cycle
-  // | -> short
-  // . -> 0 volts
-  private val brakingPatterns = List(
-    "||||||||||||", // strongest braking
-    ".|||||||||||",
-    ".|||||.|||||",
-    ".|||.|||.|||",
-    ".||.||.||.||",
-    ".|.|.||.|.||",
-    ".|.|.|.|.|.|",
-    "..|..|.|.|.|",
-    "..|..|..|..|",
-    "...|...|...|",
-    ".....|.....|",
-    "...........|",
-    "............"  // weakest braking
-  )
-    .map(_.toCharArray)
-    .map(_.map(_ == '|'))
-  private val ditherLength = brakingPatterns.map(_.length).min
+  def dynamicBrakingOutput(targetPower: Stream[Dimensionless],
+                           currentSpeed: Stream[Dimensionless]): Stream[Option[Dimensionless]] = {
+    val ditherStatus: Stream[DitherStatus] = targetPower.zip(currentSpeed).map { case (target, speed) =>
+      if ((speed.toPercent < 0) != (target.toPercent < 0)) {
+        // change in direction
+        NotBraking(target + speed)
+      } else if (target.abs < speed.abs) {
+        // we are decelerating
+        ApplyBrakingPattern(Each(target.abs / speed.abs))
+      } else {
+        NotBraking(target)
+      }
+    }
 
-  def dynamicBrakingOutput(max: ElectricPotential, free: AngularVelocity)
-                          (tick: Int, target: ElectricPotential, current: AngularVelocity)
-  : Option[ElectricPotential] = {
-    // normalized
-    val nTarget = target / max
-    val nSpeed = current / free
+    val brakingPowers = ditherStatus.map {
+      case NotBraking(_) => Percent(0)
+      case ApplyBrakingPattern(power) => power
+    }
 
-    // checks for a change of direction
-    if ((nSpeed < 0) != (nTarget < 0)) Some((nTarget + nSpeed) * max)
+    ditherStatus.zip(ditherPattern(brakingPowers)).map {
+      case (NotBraking(motorPower), _) => Some(motorPower)
+      case (ApplyBrakingPattern(_), true) => None // the pattern tells us to brake on this tick
+      case (ApplyBrakingPattern(_), false) => Some(Percent(0)) // the pattern tells us not to brake on this tick
+    }
+  }
 
-    // checks for deceleration
-    else if (0 <= nTarget.abs && nTarget.abs < nSpeed.abs)
-      if (brakingPatterns
-      (((nTarget.abs / nSpeed.abs) * brakingPatterns.length).toInt)
-      (tick.abs % ditherLength)
-      ) None
-      else Some(Volts(0))
+  def ditherPattern(brakingPowers: Stream[Dimensionless]): Stream[Boolean] = {
+    brakingPowers.scanLeft((false, 0D, 0D)) { case ((_, total, accum), requestedPower) =>
+      // results in a tuple (output, newTotal, newAccum)
 
-    else Some(target)
+      if (requestedPower == Percent(100)) {
+        (true, 0D, 0D)
+      } else if (requestedPower == Percent(0)) {
+        (false, 0D, 0D)
+      } else {
+        val windowPattern = accum / (total + 1) + (total / (1 + total))
+
+        val (inverted, requestedPowerAfterInvert) = if (requestedPower.toPercent >= 50) {
+          (false, requestedPower)
+        } else {
+          (true, Percent(100) - requestedPower) // now it's greater than 50
+        }
+
+        if (windowPattern >= requestedPowerAfterInvert.toEach) {
+          // reset if overshoot
+          (inverted, 0, (windowPattern - requestedPowerAfterInvert.toEach) * (total + 1))
+        } else {
+          (!inverted, total + 1, accum)
+        }
+      }
+    }.map(_._1)
   }
 }
