cbd7fe7a2d83164e7f56176f761c492938469b16
paxelord <philip.axelrod.web@gmail.com>
2017-12-15 14:03:29 -0800

    Add a few tests for Continuous Event logic

--- core/shared/src/test/scala/com/lynbrookrobotics/potassium/ContinuousEventTest.scala
+++ core/shared/src/test/scala/com/lynbrookrobotics/potassium/ContinuousEventTest.scala
@@ -10,7 +10,7 @@ class ContinuousEventTest extends FunSuite {
   val eventUpdateSource = new ImpulseEventSource
   implicit val eventUpdateEvent = eventUpdateSource.event
 
-  test("Start and end impulse events are fired correctly") {
+  test("Start and end impulse Stream events are fired correctly") {
     val (streamOfCondition, publishCondition) = Stream.manual[Boolean]
     publishCondition(false)
 
@@ -36,7 +36,7 @@ class ContinuousEventTest extends FunSuite {
     assert(onStart && onEnd)
   }
 
-  test("On ticking callback fired correctly") {
+  test("On ticking callback from Stream event fired correctly") {
     val (streamOfCondition, publishCondition) = Stream.manual[Boolean]
     publishCondition(false)
 
@@ -54,7 +54,7 @@ class ContinuousEventTest extends FunSuite {
     assert(callbackRun)
   }
 
-  test("Mapped continuous task launched correctly") {
+  test("Mapped continuous task from Stream event launched correctly") {
     val (streamOfCondition, publishCondition) = Stream.manual[Boolean]
     publishCondition(false)
 
@@ -87,40 +87,7 @@ class ContinuousEventTest extends FunSuite {
     assert(taskStarted && taskEnded)
   }
 
-  test("Mapped continuous task signal launched correctly") {
-    val (streamOfCondition, publishCondition) = Stream.manual[Boolean]
-    publishCondition(false)
-
-    val event = streamOfCondition.evenWithCondition(identity)
-
-    var taskStarted = false
-    var taskEnded = false
-
-    event.foreach(Signal(new ContinuousTask {
-      override def onStart(): Unit = {
-        taskStarted = true
-      }
-
-      override def onEnd(): Unit = {
-        taskEnded = true
-      }
-    }))
-
-    eventUpdateSource.fire()
-    assert(!taskStarted && !taskEnded)
-
-    publishCondition(true)
-
-    eventUpdateSource.fire()
-    assert(taskStarted && !taskEnded)
-
-    publishCondition(false)
-
-    eventUpdateSource.fire()
-    assert(taskStarted && taskEnded)
-  }
-
-  test("Combined events fired correctly") {
+  test("Combined events from Stream fired correctly") {
     val (conditionAStream, publishConditionA) = Stream.manual[Boolean]
     publishConditionA(false)
 
@@ -154,7 +121,7 @@ class ContinuousEventTest extends FunSuite {
     assert(callbackRun)
   }
 
-  test("Opposite event fired correctly") {
+  test("Opposite event from Stream fired correctly") {
     val (streamOfCondition, publishCondition) = Stream.manual[Boolean]
     publishCondition(true)
 
@@ -170,6 +137,158 @@ class ContinuousEventTest extends FunSuite {
     assert(!callbackRun)
 
     publishCondition(false)
+    eventUpdateSource.fire()
+    assert(callbackRun)
+  }
+
+  test("Start and end impulse events are fired correctly") {
+    var condition = false
+    val event = Signal(condition).filter(identity)
+
+    var onStart = false
+    var onEnd = false
+
+    event.onStart.foreach(() => onStart = true)
+    event.onEnd.foreach(() => onEnd = true)
+
+    eventUpdateSource.fire()
+    assert(!onStart && !onEnd)
+
+    condition = true
+
+    eventUpdateSource.fire()
+    assert(onStart && !onEnd)
+
+    condition = false
+
+    eventUpdateSource.fire()
+    assert(onStart && onEnd)
+  }
+
+  test("On ticking callback fired correctly") {
+    var condition = false
+    val event = Signal(condition).filter(identity)
+
+    var callbackRun = false
+    event.foreach(() => callbackRun = true)
+
+    eventUpdateSource.fire()
+    assert(!callbackRun)
+
+    condition = true
+
+    eventUpdateSource.fire()
+    assert(callbackRun)
+  }
+
+  test("Mapped continuous task launched correctly") {
+    var condition = false
+    val event = Signal(condition).filter(identity)
+
+    var taskStarted = false
+    var taskEnded = false
+
+    event.foreach(new ContinuousTask {
+      override def onStart(): Unit = {
+        taskStarted = true
+      }
+
+      override def onEnd(): Unit = {
+        taskEnded = true
+      }
+    })
+
+    eventUpdateSource.fire()
+    assert(!taskStarted && !taskEnded)
+
+    condition = true
+
+    eventUpdateSource.fire()
+    assert(taskStarted && !taskEnded)
+
+    condition = false
+
+    eventUpdateSource.fire()
+    assert(taskStarted && taskEnded)
+  }
+
+  test("Mapped continuous task signal launched correctly") {
+    var condition = false
+    val event = Signal(condition).filter(identity)
+
+    var taskStarted = false
+    var taskEnded = false
+
+    event.foreach(Signal(new ContinuousTask {
+      override def onStart(): Unit = {
+        taskStarted = true
+      }
+
+      override def onEnd(): Unit = {
+        taskEnded = true
+      }
+    }))
+
+    eventUpdateSource.fire()
+    assert(!taskStarted && !taskEnded)
+
+    condition = true
+
+    eventUpdateSource.fire()
+    assert(taskStarted && !taskEnded)
+
+    condition = false
+
+    eventUpdateSource.fire()
+    assert(taskStarted && taskEnded)
+  }
+
+  test("Combined events fired correctly") {
+    var conditionA = false
+    var conditionB = false
+
+    var callbackRun = false
+
+    val eventA = Signal(conditionA).filter(identity)
+    val eventB = Signal(conditionB).filter(identity)
+
+    (eventA && eventB).foreach(() => {
+      callbackRun = true
+    })
+
+    eventUpdateSource.fire()
+    assert(!callbackRun)
+
+    conditionA = true
+    eventUpdateSource.fire()
+    assert(!callbackRun)
+
+    conditionA = false
+    conditionB = true
+    eventUpdateSource.fire()
+    assert(!callbackRun)
+
+    conditionA = true
+    conditionB = true
+    eventUpdateSource.fire()
+    assert(callbackRun)
+  }
+
+  test("Opposite event fired correctly") {
+    var condition = true
+
+    var callbackRun = false
+
+    val event = Signal(condition).filter(identity)
+
+    (!event).foreach(() => {
+      callbackRun = true
+    })
+
+    eventUpdateSource.fire()
+    assert(!callbackRun)
+
+    condition = false
     eventUpdateSource.fire()
     assert(callbackRun)
   }
