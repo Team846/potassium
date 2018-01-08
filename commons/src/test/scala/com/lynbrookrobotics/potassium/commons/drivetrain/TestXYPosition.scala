a02613487bd1edc9403a7099ffad2afa3c085afb
nikwalia <nikash.walia@gmail.com>
2018-01-07 17:57:33 -0800

    unit test added

--- commons/src/test/scala/com/lynbrookrobotics/potassium/commons/drivetrain/TestXYPosition.scala
+++ commons/src/test/scala/com/lynbrookrobotics/potassium/commons/drivetrain/TestXYPosition.scala
@@ -30,12 +30,15 @@ class TestXYPosition extends FunSuite{
     val simpsonsPosition = XYPosition.positionWithSimpsons(
       angle,
       velocity)
+    val circularPosition = XYPosition.circularTracking(angle, distance)
 
     var lastPosition = unitializedPose
     var lastSimpsonPosition = unitializedPose
+    var lastCircularPosition = unitializedPose
 
     position.foreach(lastPosition = _)
     simpsonsPosition.foreach(lastSimpsonPosition = _)
+    circularPosition.foreach(lastCircularPosition = _)
 
     for(_ <- 1 to periodsPerSecond){
       clockTrigger.apply(period)
@@ -46,6 +49,7 @@ class TestXYPosition extends FunSuite{
     implicit val tolerance = Feet(0.1)
     assert(lastPosition ~= targetPosition)
     assert(lastSimpsonPosition ~= targetPosition)
+    assert(lastCircularPosition ~= targetPosition)
   }
 
   test("Test moving 45 degrees at 1 ft/s for 1 sec results in (sqrt(2)/2,sqrt(2)/2) ") {
@@ -63,12 +67,17 @@ class TestXYPosition extends FunSuite{
     val simpsonsPosition = XYPosition.positionWithSimpsons(
       angle,
       velocity)
+    val circularPosition = XYPosition.circularTracking(
+      angle, distance
+    )
 
     var lastPose = unitializedPose
     var lastSimpsonsPose = unitializedPose
+    var lastCircularPose = unitializedPose
 
     position.foreach(lastPose = _)
     simpsonsPosition.foreach(lastSimpsonsPose = _)
+    circularPosition.foreach(lastCircularPose = _)
 
     for (_ <- 1 to periodsPerSecond) {
       clockTrigger.apply(period)
@@ -79,6 +88,7 @@ class TestXYPosition extends FunSuite{
     implicit val tolerance = Feet(0.1)
     assert(lastPose ~= targetPosition)
     assert(lastSimpsonsPose ~= targetPosition)
+    assert(lastCircularPose ~= targetPosition)
   }
 
   test("Test rotating 90 degrees/s with radius 1 starting angle 90 " +
@@ -89,6 +99,9 @@ class TestXYPosition extends FunSuite{
 
     val (angularSpeed, pubTurnSpeed) = Stream.manualWithTime[AngularVelocity](Periodic(period))
     val angle = angularSpeed.integral.map(_ + initialAngle)
+
+    val (straightVelocity, pubStraightVelocity) = Stream.manualWithTime[Velocity](Periodic(period))
+    val straightDistance = straightVelocity.integral
 
     val distance = angle.map{ angle =>
       (angle - initialAngle) onRadius Feet(1)
@@ -104,12 +117,15 @@ class TestXYPosition extends FunSuite{
     val simpsonsPosition = XYPosition.positionWithSimpsons(
       angle,
       velocity)
+    val circularPosition = XYPosition.circularTracking(angle, straightDistance)
 
     var lastPose = unitializedPose
     var lastSimpPose = unitializedPose
+    var lastCircularPose = unitializedPose
 
     position.foreach(lastPose = _)
     simpsonsPosition.foreach(lastSimpPose = _)
+    circularPosition.foreach(lastCircularPose = _)
 
     for(_ <- 1 to periodsPerSecond){
       clockTrigger.apply(period)
@@ -119,5 +135,6 @@ class TestXYPosition extends FunSuite{
     implicit val tolerance = Feet(0.1)
     assert(lastPose ~= targetPosition)
     assert(lastSimpPose ~= targetPosition)
+    assert(lastCircularPose ~= targetPosition)
   }
 }
