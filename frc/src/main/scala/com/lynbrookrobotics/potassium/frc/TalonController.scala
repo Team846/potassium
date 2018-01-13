7cbfd462cf10f2e9033d29b1fde5efef53fb22e8
Sang Hak (Andy) Chun <sanghak@andychun.info>
2018-01-12 19:13:14 -0800

    Added method to set percent output

--- frc/src/main/scala/com/lynbrookrobotics/potassium/frc/TalonController.scala
+++ frc/src/main/scala/com/lynbrookrobotics/potassium/frc/TalonController.scala
@@ -1,6 +1,6 @@
 package com.lynbrookrobotics.potassium.frc
 
-import com.ctre.phoenix.motorcontrol.{FeedbackDevice, VelocityMeasPeriod}
+import com.ctre.phoenix.motorcontrol.{ControlMode, FeedbackDevice, VelocityMeasPeriod}
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 
 class TalonController(deviceNumber: Int) {
@@ -11,6 +11,8 @@ class TalonController(deviceNumber: Int) {
   talon.configVelocityMeasurementWindow(64, 0)
 
   def follow (talonController: TalonController): Unit = talon.follow(talonController.talon)
+
+  def setOutput (output: Double): Unit = talon.set(ControlMode.PercentOutput, output)
 
   def rawPosition: Int = talon.getSelectedSensorPosition(0)
   def rawVelocity: Int = talon.getSelectedSensorVelocity(0)
