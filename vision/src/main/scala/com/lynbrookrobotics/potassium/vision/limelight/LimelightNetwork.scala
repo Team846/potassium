3057ba78de8853cf02d931a7cfb0e5edb5384b94
JoshZF <joshz.fong@gmail.com>
2018-01-14 15:46:31 -0800

    target tracking

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/limelight/LimelightNetwork.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/limelight/LimelightNetwork.scala
@@ -1,17 +1,18 @@
 package com.lynbrookrobotics.potassium.vision.limelight
 
+import com.lynbrookrobotics.potassium.clock.Clock
 import com.lynbrookrobotics.potassium.streams._
 import edu.wpi.first.networktables.{NetworkTable, NetworkTableInstance}
-import squants.{Dimensionless, Percent}
 import squants.space.{Angle, Degrees}
 import squants.time.Milliseconds
 
-object LimelightNetwork {
-
-  val table: NetworkTableInstance = (new NetworkTable).getInstance()
+class LimelightNetwork(table: NetworkTable)(implicit clock: Clock) {
   val yOffsetAngle: Stream[Angle] = Stream.periodic(Milliseconds(5))(Degrees(table.getEntry("tx").getDouble(0)))
   val xOffsetAngle: Stream[Angle] = Stream.periodic(Milliseconds(5))(Degrees(table.getEntry("ty").getDouble(0)))
-  val targetArea: Stream[Dimensionless] = Stream.periodic(Milliseconds(5))(Percent(table.getEntry("ta").getDouble(0)))
-  val targetSkew: Stream[Dimensionless] = Stream.periodic(Milliseconds(5))(Percent(table.getEntry("ts").getDouble(0)))
+}
 
+object LimelightNetwork{
+  def apply(implicit clock: Clock): LimelightNetwork = {
+    new LimelightNetwork(NetworkTableInstance.create().getTable("limelight"))
+  }
 }
