4180825ff5f449a7f5aad1c36fd1c934a2f7d2c5
JoshZF <joshz.fong@gmail.com>
2018-01-13 16:22:11 -0800

    initial data retrieval

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/limelight/LimelightNetwork.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/limelight/LimelightNetwork.scala
@@ -1,19 +1,15 @@
 package com.lynbrookrobotics.potassium.vision.limelight
 
-import com.lynbrookrobotics.potassium.clock.Clock
 import com.lynbrookrobotics.potassium.streams._
 import edu.wpi.first.networktables.{NetworkTable, NetworkTableInstance}
 import squants.{Dimensionless, Percent}
 import squants.space.{Angle, Degrees}
 import squants.time.Milliseconds
 
-class LimelightNetwork(table: NetworkTable)(implicit clock: Clock) {
-  val xOffsetAngle: Stream[Angle] = Stream.periodic(Milliseconds(5))(Degrees(table.getEntry("tx").getDouble(0)))
-  val percentArea: Stream[Dimensionless] = Stream.periodic(Milliseconds(5))(Percent(table.getEntry("ta").getDouble(0)))
-}
-
-object LimelightNetwork{
-  def apply(implicit clock: Clock): LimelightNetwork = {
-    new LimelightNetwork(NetworkTableInstance.getDefault.getTable("/limelight"))
-  }
+class LimelightNetwork {
+  val table: NetworkTableInstance = (new NetworkTable).getInstance()
+  val targetOffsetAngle_Horizontal: Stream[Angle] = Stream.periodic(Milliseconds(5))(Degrees(table.getEntry("tx").getDouble(0)))
+  val targetOffsetAngle_Vertical: Stream[Angle] = Stream.periodic(Milliseconds(5))(Degrees(table.getEntry("ty").getDouble(0)))
+  val targetArea: Stream[Dimensionless] = Stream.periodic(Milliseconds(5))(Percent(table.getEntry("ta").getDouble(0)))
+  val targetSkew: Stream[Dimensionless] = Stream.periodic(Milliseconds(5))(Percent(table.getEntry("ts").getDouble(0)))
 }
