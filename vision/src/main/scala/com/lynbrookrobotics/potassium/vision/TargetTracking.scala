86b8dd3524daf563a798d1526bc0ac9772727d18
JoshZF <joshz.fong@gmail.com>
2018-01-15 18:48:38 -0800

    added unit tests

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/TargetTracking.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/TargetTracking.scala
@@ -2,19 +2,18 @@ package com.lynbrookrobotics.potassium.vision
 
 import com.lynbrookrobotics.potassium.streams.Stream
 import com.lynbrookrobotics.potassium.units.Point
-import com.lynbrookrobotics.potassium.vision.limelight.{CameraProperties, LimelightNetwork}
+import com.lynbrookrobotics.potassium.vision.limelight.CameraProperties
 import squants.Length
-import squants.space.{Angle, Degrees, Feet}
+import squants.space.{Angle, Feet}
 
 
-class TargetTracking(implicit limelightNetwork: LimelightNetwork, props: CameraProperties) {
-  val target: Stream[Point] = {
-    limelightNetwork.xOffsetAngle.zip(limelightNetwork.yOffsetAngle).map { case (xOffsetAngle: Angle, yOffsetAngle: Angle) => {
-        val d: Length = Feet(props.targetHeight.value - props.cameraHeight.value) / math.tan(props.cameraVerticalOffset.value + yOffsetAngle.value)
-        val ang: Angle = Degrees(xOffsetAngle.value - props.cameraHorizontalOffset.value)
-
-        Point(x = d * math.cos(ang.value),y = d * math.cos(ang.value))
-      }
+class TargetTracking(val offsets: Stream[(Angle, Angle)])(implicit props: CameraProperties) {
+  def target: Stream[Point] = {
+    offsets.map { case (xOffsetAngle: Angle, yOffsetAngle: Angle) =>
+      val ratio: Double = (props.cameraVerticalOffset + yOffsetAngle).tan
+      val d: Length = Feet(props.targetHeight.value - props.cameraHeight.value) / ratio
+      val ang: Angle = xOffsetAngle + props.cameraHorizontalOffset
+      Point(x = d * ang.sin, y = d * ang.cos)
     }
   }
 }
