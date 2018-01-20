9fa829a427fec57a5bf6fa99b94796f3e7502c90
JoshZF <joshz.fong@gmail.com>
2018-01-20 14:58:02 -0800

    got data

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/TargetTracking.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/TargetTracking.scala
@@ -5,7 +5,6 @@ import com.lynbrookrobotics.potassium.vision.limelight.CameraProperties
 import squants.{Dimensionless, Length}
 import squants.space.{Angle, Feet}
 
-
 class TargetTracking(val xOffset: Stream[Angle], val percentArea: Stream[Dimensionless])(implicit props: CameraProperties) {
   val distanceToTarget: Stream[Length] = {
     percentArea.map(r => Feet(10.6845 / math.sqrt(r.value)))
