970071eeb1421d96ce702c628fa6b0390c281e52
JoshZF <joshz.fong@gmail.com>
2018-01-20 14:58:02 -0800

    got data

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/TargetTracking.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/TargetTracking.scala
@@ -1,19 +1,18 @@
 package com.lynbrookrobotics.potassium.vision
 
 import com.lynbrookrobotics.potassium.streams.Stream
-import com.lynbrookrobotics.potassium.units.Point
 import com.lynbrookrobotics.potassium.vision.limelight.CameraProperties
 import squants.Length
 import squants.space.{Angle, Feet}
 
 
 class TargetTracking(val offsets: Stream[(Angle, Angle)])(implicit props: CameraProperties) {
-  def target: Stream[Point] = {
+  def target: Stream[(Length, Angle)] = {
     offsets.map { case (xOffsetAngle: Angle, yOffsetAngle: Angle) =>
       val ratio: Double = (props.cameraVerticalOffset + yOffsetAngle).tan
       val d: Length = Feet(props.targetHeight.value - props.cameraHeight.value) / ratio
       val ang: Angle = xOffsetAngle + props.cameraHorizontalOffset
-      Point(x = d * ang.sin, y = d * ang.cos)
+      (d, ang)
     }
   }
 }
