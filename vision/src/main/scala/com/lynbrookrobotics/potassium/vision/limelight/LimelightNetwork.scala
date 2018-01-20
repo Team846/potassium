af2c5326e17e42da20fe3c5d4e3075c42ea3d6a2
JoshZF <joshz.fong@gmail.com>
2018-01-20 14:58:02 -0800

    got data

--- vision/src/main/scala/com/lynbrookrobotics/potassium/vision/limelight/LimelightNetwork.scala
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/limelight/LimelightNetwork.scala
@@ -7,12 +7,14 @@ import squants.space.{Angle, Degrees}
 import squants.time.Milliseconds
 
 class LimelightNetwork(table: NetworkTable)(implicit clock: Clock) {
+  val hasTarget: Stream[Double] = Stream.periodic(Milliseconds(5))(table.getEntry("tv").getDouble(0))
   val yOffsetAngle: Stream[Angle] = Stream.periodic(Milliseconds(5))(Degrees(table.getEntry("tx").getDouble(0)))
   val xOffsetAngle: Stream[Angle] = Stream.periodic(Milliseconds(5))(Degrees(table.getEntry("ty").getDouble(0)))
+  val keys = table.getKeys()
 }
 
 object LimelightNetwork{
   def apply(implicit clock: Clock): LimelightNetwork = {
-    new LimelightNetwork(NetworkTableInstance.create().getTable("limelight"))
+    new LimelightNetwork(NetworkTableInstance.getDefault.getTable("/limelight"))
   }
 }
