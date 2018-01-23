119b768c13370606537784534254245d2dc4ee83
Nithya <hi2nithya@gmail.com>
2018-01-22 19:30:24 -0800

    fixed even more things (should actually compile now)

--- core/shared/src/main/scala/com/lynbrookrobotics/potassium/units/Histogram.scala
+++ core/shared/src/main/scala/com/lynbrookrobotics/potassium/units/Histogram.scala
@@ -7,11 +7,11 @@ import java.util.function.DoubleConsumer
   * Created by the-magical-llamicorn on 4/19/17.
   */
 case class Histogram(min: Time, max: Time, bins: Int) {
+  final var interval: Time = Milliseconds(.0)
   interval = ((max) - min) / bins
+  final protected var histogram: Array[Long] = null
   histogram = new Array[Long](bins + 2)
   // bins and "less than min" and "greater than max"
-  final protected var histogram: Array[Long] = null
-  final var interval: Time = Milliseconds(.0)
 
   def apply(value: Time) {
     if (value < min) {
@@ -33,8 +33,8 @@ case class Histogram(min: Time, max: Time, bins: Int) {
     sb.append(" : ")
     sb.append(histogram(0))
     sb.append('\n')
-    var i: Int = 1
-    while (i < bins + 1) {
+
+    for  { i <- 1 to bins - 1 }
       {
         sb.append((min + (interval * (i - 1))))
         sb.append(" to ")
@@ -43,15 +43,12 @@ case class Histogram(min: Time, max: Time, bins: Int) {
         sb.append(histogram(i))
         sb.append('\n')
       }
-      {
-        i += 1; i - 1
-      }
-    }
+
     sb.append(">")
     sb.append(max)
     sb.append(" : ")
     sb.append(histogram(bins + 1))
     sb.append('\n')
-    return sb.toString
+    sb.toString
   }
 }
