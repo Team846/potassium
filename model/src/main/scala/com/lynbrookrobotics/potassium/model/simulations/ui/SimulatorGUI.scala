8a3d9c59cf875b871286579743d7664a0c6c8fdf
Aanand Kainth <akainth015@gmail.com>
2018-01-29 18:28:28 -0800

    Updates to the code

--- model/src/main/scala/com/lynbrookrobotics/potassium/model/simulations/ui/SimulatorGUI.scala
+++ model/src/main/scala/com/lynbrookrobotics/potassium/model/simulations/ui/SimulatorGUI.scala
@@ -3,21 +3,34 @@ package com.lynbrookrobotics.potassium.model.simulations.ui
 import java.awt.Dimension
 import javax.swing.JFrame
 
-import squants.space.Length
+import com.lynbrookrobotics.potassium.units.Point
+import squants.space.{Angle, Length}
 
 class SimulatorGUI(val fieldWidth: Length,
                    val fieldHeight: Length,
                    val boxSize: Int = 50,
                    val maxSize: Dimension = new Dimension(600, 400)) {
+
+  val frameWidth: Int = Math.min(fieldWidth.toFeet.toInt * boxSize, maxSize.width)
+  val frameHeight: Int = Math.min(fieldHeight.toFeet.toInt * boxSize, maxSize.height)
+
+  val canvas = new Canvas(frameWidth, frameHeight)
+
+  val frame = new JFrame("Simulator")
+  frame.setContentPane(canvas)
+  frame.setSize(new Dimension(frameWidth, frameHeight))
+
+  def pointToPixels(p: Point): (Int, Int) = {
+    (frameWidth / fieldWidth.toFeet.toInt * p.x.toFeet.toInt, frameHeight / fieldHeight.toFeet.toInt * p.y.toFeet.toInt)
+  }
+
   def startGUI(): Unit = {
-    var frameWidth: Int = Math.min(fieldWidth.toFeet.toInt * boxSize, maxSize.width)
-    var frameHeight: Int = Math.min(fieldHeight.toFeet.toInt * boxSize, maxSize.height)
-
-    val canvas = new Canvas(frameWidth, frameHeight)
-
-    val frame = new JFrame("Simulator")
-    frame.setContentPane(canvas)
-    frame.setSize(new Dimension(frameWidth, frameHeight))
     frame.setVisible(true)
+  }
+
+  def update(p: Point, a: Angle): Unit = {
+    val (x, y) = pointToPixels(p)
+    canvas.setPoint(x, y)
+    canvas.setAngle(a)
   }
 }
--- model/src/test/scala/com/lynbrookrobotics/potassium/model/simulations/ui/SimulatorTest.scala
+++ model/src/test/scala/com/lynbrookrobotics/potassium/model/simulations/ui/SimulatorTest.scala
@@ -1,11 +1,13 @@
 package com.lynbrookrobotics.potassium.model.simulations.ui
 
+import com.lynbrookrobotics.potassium.units.Point
 import org.scalatest.FunSuite
-import squants.space.Feet
+import squants.space.{Degrees, Feet}
 
 class SimulatorTest extends FunSuite {
-  test("Check that the simulator GUI starts up, and can change the canvas") {
+  test("Test GUI") {
     val simulatorGUI = new SimulatorGUI(Feet(10), Feet(10))
     simulatorGUI.startGUI()
+    simulatorGUI.update(Point(Feet(1), Feet(1)), Degrees(45))
   }
 }
