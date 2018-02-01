7a32aab530f1e7473f95fc9cbe240ba97979e2b3
nikwalia <nikash.walia@gmail.com>
2018-01-31 19:47:41 -0800

    refactoring code

---
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/VisionTargetTracking.scala
@@ -0,0 +1,23 @@
+package com.lynbrookrobotics.potassium.vision
+
+import com.lynbrookrobotics.potassium.Signal
+import com.lynbrookrobotics.potassium.streams.Stream
+import com.lynbrookrobotics.potassium.vision.limelight.VisionProperties
+import squants.{Dimensionless, Length}
+import squants.space.Angle
+
+object VisionTargetTracking {
+  def distanceToTarget(percentArea: Stream[Option[Dimensionless]])
+                      (implicit props: Signal[VisionProperties]): Stream[Option[Length]] = {
+    percentArea.map ( p =>
+      p.map { percentArea =>
+        props.get.distanceConstant / math.sqrt(percentArea.toPercent)
+      }
+    )
+  }
+
+  def angleToTarget(xOffset: Stream[Angle])
+                   (implicit props: Signal[VisionProperties]): Stream[Angle] = {
+    xOffset.map(_ + props.get.cameraHorizontalOffset)
+  }
+}
---
+++ vision/src/main/scala/com/lynbrookrobotics/potassium/vision/limelight/VisionProperties.scala
@@ -0,0 +1,6 @@
+package com.lynbrookrobotics.potassium.vision.limelight
+
+import squants.Angle
+import squants.space.Length
+
+case class VisionProperties(cameraHorizontalOffset: Angle, distanceConstant: Length)
