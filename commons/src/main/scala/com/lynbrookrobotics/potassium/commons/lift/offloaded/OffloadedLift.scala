a5c88a87de8c5d3ac5bf94f092b7ee2281665bab
Kunal Sheth <kunal@kunalsheth.info>
2018-02-04 13:36:52 -0800

    wrote offloaded bangbang controllers

--- commons/src/main/scala/com/lynbrookrobotics/potassium/commons/lift/offloaded/OffloadedLift.scala
+++ commons/src/main/scala/com/lynbrookrobotics/potassium/commons/lift/offloaded/OffloadedLift.scala
@@ -2,12 +2,12 @@ package com.lynbrookrobotics.potassium.commons.lift.offloaded
 
 import com.lynbrookrobotics.potassium.Signal
 import com.lynbrookrobotics.potassium.commons.lift.Lift
+import com.lynbrookrobotics.potassium.control.offload.EscConfig._
 import com.lynbrookrobotics.potassium.control.offload.OffloadedSignal
-import com.lynbrookrobotics.potassium.control.offload.OffloadedSignal.{OpenLoop, PositionPID}
+import com.lynbrookrobotics.potassium.control.offload.OffloadedSignal.{OpenLoop, PositionBangBang, PositionPID}
 import com.lynbrookrobotics.potassium.streams._
 import squants.Dimensionless
 import squants.space.Length
-import com.lynbrookrobotics.potassium.control.offload.EscConfig._
 
 abstract class OffloadedLift extends Lift {
   override type LiftSignal = OffloadedSignal
@@ -26,6 +26,40 @@ abstract class OffloadedLift extends Lift {
       implicit val c = curProps.escConfig
       PositionPID(
         forwardToAngularPositionGains(curProps.positionGains), ticks(it)
+      )
+    }
+  )
+
+  def stayAbove(target: Stream[Length])
+               (implicit properties: Signal[Properties],
+                hardware: Hardware): (Stream[Length], Stream[LiftSignal]) = (
+    hardware.
+      position.
+      zipAsync(target)
+      .map(t => t._2 - t._1),
+
+    target.map { it =>
+      implicit val c = properties.get.escConfig
+      PositionBangBang(
+        forwardWhenBelow = true, reverseWhenAbove = false,
+        signal = ticks(it)
+      )
+    }
+  )
+
+  def stayBelow(target: Stream[Length])
+               (implicit properties: Signal[Properties],
+                hardware: Hardware): (Stream[Length], Stream[LiftSignal]) = (
+    hardware.
+      position.
+      zipAsync(target)
+      .map(t => t._2 - t._1),
+
+    target.map { it =>
+      implicit val c = properties.get.escConfig
+      PositionBangBang(
+        forwardWhenBelow = false, reverseWhenAbove = true,
+        signal = ticks(it)
       )
     }
   )
