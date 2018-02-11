15b5e6855563e49208054ccba370c6e6e62d8a06
Ankit Lakkapragada <ankit.lakkapragada@gmail.com>
2018-02-10 17:27:39 -0800

    We got it to work.

--- model/src/main/scala/com/lynbrookrobotics/potassium/model/simulations/ui/DataChunk.scala
+++ model/src/main/scala/com/lynbrookrobotics/potassium/model/simulations/ui/DataChunk.scala
@@ -3,4 +3,4 @@ package com.lynbrookrobotics.potassium.model.simulations.ui
 import com.lynbrookrobotics.potassium.units.Point
 import squants.{Angle, Time}
 
-case class DataChunk(time: Time, point: Point, angle: Angle)
+case class DataChunk(val time: Time, val point: Point, val angle: Angle)
