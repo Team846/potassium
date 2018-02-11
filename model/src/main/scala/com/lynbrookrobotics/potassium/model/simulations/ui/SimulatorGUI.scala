2ba82f74ed49cffa52121d5895e99b4ef54e462d
Ankit Lakkapragada <ankit.lakkapragada@gmail.com>
2018-02-10 19:57:54 -0800

    Made it neater and more legible.

--- model/src/main/scala/com/lynbrookrobotics/potassium/model/simulations/ui/SimulatorGUI.scala
+++ model/src/main/scala/com/lynbrookrobotics/potassium/model/simulations/ui/SimulatorGUI.scala
@@ -1,56 +1,64 @@
 package com.lynbrookrobotics.potassium.model.simulations.ui
 
-import java.awt.{BorderLayout, Color, Dimension}
+import java.awt.{BorderLayout, Dimension}
 import java.io.File
 import java.util
 import java.util.Scanner
 import javax.swing.event.{ChangeEvent, ChangeListener}
-import javax.swing.{JFrame, JPanel, JSlider}
+import javax.swing.{JFrame, JSlider}
 
 import com.lynbrookrobotics.potassium.units.Point
 import squants.space.{Angle, Degrees, Feet, Length}
 import squants.time.Seconds
-
-import scala.collection.mutable
 
 class SimulatorGUI(val fieldWidth: Length,
                    val fieldHeight: Length,
                    val boxSize: Int = 50,
                    val maxSize: Dimension = new Dimension(600, 400)) {
 
-  var slider: JSlider = _
+  /**
+    * This is the frame size, in pixels
+    */
+  val frameSize = new Dimension(
+    Math.min(fieldWidth.toFeet.toInt * boxSize, maxSize.width),
+    Math.min(fieldHeight.toFeet.toInt * boxSize, maxSize.height))
 
-  var data: util.ArrayList[(Double, DataChunk)] = _
+  /**
+    * This is the Canvas (customized) which we draw with
+    */
+  val canvas = new Canvas(frameSize.width, frameSize.height)
 
-
-  val frameWidth: Int = Math.min(fieldWidth.toFeet.toInt * boxSize, maxSize.width)
-  val frameHeight: Int = Math.min(fieldHeight.toFeet.toInt * boxSize, maxSize.height)
-
-  val canvas = new Canvas(frameWidth, frameHeight)
-
-  val container = new JPanel()
-  container.setLayout(new BorderLayout())
-  container.add(canvas)
-
+  /**
+    * This is the frame for our simulator
+    */
   val frame = new JFrame("Simulator")
-  frame.setContentPane(container)
-  frame.setSize(new Dimension(frameWidth, frameHeight))
 
+  private val timeSlider = new JSlider()
+  private val layout = new BorderLayout()
 
-  def pointToPixels(p: Point): (Int, Int) = {
-    (frameWidth / fieldWidth.toFeet.toInt * p.x.toFeet.toInt, frameHeight / fieldHeight.toFeet.toInt * p.y.toFeet.toInt)
-  }
+  /**
+    * This contains the data read from a log file
+    */
+  private var data: util.ArrayList[(Double, DataChunk)] = _
 
-  def startGUI(): Unit = {
-    frame.setVisible(true)
-  }
+  /*****************
+   * CONFIGURATION *
+   *****************/
+  frame.setLayout(layout)
+  frame.setSize(frameSize)
 
-  def update(p: Point, a: Angle): Unit = {
-    val (x, y) = pointToPixels(p)
-    canvas.setPoint(x, y)
-    canvas.setAngle(a)
-    canvas.repaint()
-  }
+  timeSlider.setMinimum(0)
+  timeSlider.setValue(0)
+  timeSlider.addChangeListener(new ChangeListener {
+    override def stateChanged(e: ChangeEvent): Unit = {
+      val (_, dataChunk) = data.get(timeSlider.getValue)
+      update(dataChunk.point, dataChunk.angle)
+    }
+  })
+  timeSlider.setToolTipText("Time")
+
+  layout.addLayoutComponent(timeSlider, BorderLayout.SOUTH)
+  layout.addLayoutComponent(canvas, BorderLayout.CENTER)
 
   def useLogFile(log: File, doNotLoadIntoMemory: Boolean = false): Unit = {
     val scanner = new Scanner(log)
@@ -63,25 +71,23 @@ class SimulatorGUI(val fieldWidth: Length,
         Degrees(line(4).toDouble))
       times.add((dataChunk.time.toSeconds, dataChunk))
     }
-    data = times
-    var hadSlider = false
-    slider = if (slider == null) new JSlider() else {
-      hadSlider = true
-      slider
+    if (data != null) {
+      timeSlider.setMaximum(times.size)
     }
-    slider.setMinimum(0)
-    slider.setMaximum(data.size)
-    slider.setValue(0)
-    if (!hadSlider) {
-      container.add(slider)
-      slider.addChangeListener(new ChangeListener {
-        override def stateChanged(e: ChangeEvent): Unit = {
-          println(data.size)
-          val (_, dataChunk) = data.get(slider.getValue)
-          update(dataChunk.point, dataChunk.angle)
-        }
-      })
+  }
 
-    }
+  def pointToPixels(p: Point): (Int, Int) = {
+    (frameSize.width / fieldWidth.toFeet.toInt * p.x.toFeet.toInt, frameSize.height / fieldHeight.toFeet.toInt * p.y.toFeet.toInt)
+  }
+
+  def startGUI(): Unit = {
+    frame.setVisible(true)
+  }
+
+  def update(p: Point, a: Angle): Unit = {
+    val (x, y) = pointToPixels(p)
+    canvas.setPoint(x, y)
+    canvas.setAngle(a)
+    canvas.repaint()
   }
 }
