3135a3c200d2ddc5af3f61d2f8bd222d1d07f6a8
Aanand Kainth <akainth015@gmail.com>
2018-02-13 15:38:24 -0800

    Edited SBT version, and correct ability to drive in only one direction

---
+++ commons/src/test/scala/com/lynbrookrobotics/potassium/commons/drivetrain/twoSided/BusDrivingTest.scala
@@ -0,0 +1,9 @@
+package com.lynbrookrobotics.potassium.commons.drivetrain.twoSided
+
+import org.scalatest.FunSuite
+
+class BusDrivingTest extends FunSuite {
+  test("Test that radius is correct") {
+
+  }
+}
