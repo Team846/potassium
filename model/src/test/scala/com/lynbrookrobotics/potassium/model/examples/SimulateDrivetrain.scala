d31d4a4e8a6411d1813396bca2266c00f1388028
Shadaj Laddad <shadaj@users.noreply.github.com>
2018-03-05 18:16:04 -0800

    Make simulation tests compile

--- model/src/test/scala/com/lynbrookrobotics/potassium/model/examples/SimulateDrivetrain.scala
+++ model/src/test/scala/com/lynbrookrobotics/potassium/model/examples/SimulateDrivetrain.scala
@@ -27,6 +27,7 @@ object SimulateDrivetrain extends App {
 
     override val maxTurnVelocity: AngularVelocity = DegreesPerSecond(10)
     override val maxAcceleration: Acceleration = FeetPerSecondSquared(16.5)
+    override val maxDeceleration: Acceleration = FeetPerSecondSquared(16.5)
     override val defaultLookAheadDistance: Length = Feet(1)
 
     override val turnVelocityGains: TurnVelocityGains = PIDConfig(
